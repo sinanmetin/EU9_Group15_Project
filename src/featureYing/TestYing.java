ce8a8ffc2a6a5b26d63b3ff5b2faffd66cc0c4ae
ying <annchen20092009@hotmail.com>
2022-07-31 22:47:13 +0200

    some changes

--- src/featureYing/TestYing.java
+++ src/featureYing/TestYing.java
@@ -5,6 +5,6 @@ public class TestYing {
     public static void main(String[] args) {
 
         System.out.println("Hey guy, here i am!");
-        System.out.println("I am still no sure about it");
+        System.out.println("I am still not sure about it");
     }
 }
