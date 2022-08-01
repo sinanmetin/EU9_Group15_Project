ba16fdf4fd35a17573bc08ade1e087e87b705c97
Yimamu Abudouweili <yimamu01@gmail.com>
2022-08-01 19:08:06 +0200

    lets practice

---
+++ src/Feature_Abduwali/Hello.java
@@ -0,0 +1,7 @@
+package Feature_Abduwali;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello EveryOne");
+    }
+}
---
+++ src/Feature_Abduwali/Practice.java
@@ -0,0 +1,7 @@
+package Feature_Abduwali;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Let us practice Git and Gitup");
+    }
+}
