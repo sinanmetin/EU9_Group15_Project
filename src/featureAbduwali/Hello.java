b0611d10408b53731684378e415436557756c5b8
ying <annchen20092009@hotmail.com>
2022-08-01 15:58:07 +0200

    welcome Veli

--- src/featureAbduwali/Hello.java
+++ src/featureAbduwali/Hello.java
@@ -3,5 +3,7 @@ package featureAbduwali;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Everyone");
+
+        System.out.println("Welcome to Git,Veli!");
     }
 }
